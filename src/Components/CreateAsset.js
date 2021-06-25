6d456a06a2816c1d7742b791693e511aa96d4da1
Viraj Patidar <vrj381@gmail.com>
2021-06-25 12:56:51 +0530

    create user implemented in CreateAsset page

--- src/Components/CreateAsset.js
+++ src/Components/CreateAsset.js
@@ -25,12 +25,52 @@ const CreateAsset = () => {
     //LOGIN  ==============================
 
     const [accountAd, setaccountAd] = useState("")
+    const [iscreate, setiscreate] = useState(false)
 
+    function createUser(accAd) {
+        axios.get('http://localhost:5000/api/user/' + accAd)
+        .then(res => {
+            console.log(res)
+            if(res.data.data == null){
+                console.log({
+                    "username":"User_" + accAd.substring(accAd.length - 5),
+                    "account_address":[accAd],
+                    "user_type":"",
+                    "bio":"",
+                    "email_address":"",
+                    "bg_img_url":"https://ipfs.io/ipfs/QmSpr3ACDG1SoY7dLGns1wHUp94fMu6CvE8FAkvTnjzBtD",
+                    "profile_pic_url":"https://ipfs.io/ipfs/QmaZS9UiC9vbxUaEze3Kt4dCLH74CUCb23YoSfxp1BzM2J",
+                    "is_verified":true,
+                    "is_deleted":false
+                })
+                axios.post('http://localhost:5000/api/user/',
+                    {
+                        "username":"User_" + accAd.substring(accAd.length - 5),
+                        "account_address":[accAd],
+                        "user_type":"",
+                        "bio":"",
+                        "email_address":"",
+                        "bg_img_url":"https://ipfs.io/ipfs/QmSpr3ACDG1SoY7dLGns1wHUp94fMu6CvE8FAkvTnjzBtD",
+                        "profile_pic_url":"https://ipfs.io/ipfs/QmaZS9UiC9vbxUaEze3Kt4dCLH74CUCb23YoSfxp1BzM2J",
+                        "is_verified":true,
+                        "is_deleted":false
+                    }
+                )
+                .then(res => {
+                    console.log(res);
+                    setiscreate(true);
+                })
+                .catch(err => {console.log(err)} );
+            }
+        })
+        .catch(err => {console.log(err)} );
+    }
 
     async function enableEthereum() {
         const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
         const account = accounts[0];
         setaccountAd(account);
+        createUser(account);
         console.log(account);
     }
 
