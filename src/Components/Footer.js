fd116327cc1167bb764331b7e7e3c9394cfea887
sid507 <ssm2@somaiya.edu>
2021-06-30 10:34:44 +0530

    'footer_correction_header'

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,82 +7,81 @@ import FOOTER_LINKS from '../Mock_Api/footer_links.json';
 // import MailOutlineIcon from '@material-ui/icons/MailOutline';
 
 
-const Links = ({title,links}) => {
-        
+const Links = ({ title, links }) => {
+
     // console.log(props);
-    return (    
-             <div className="flex flex-col p-t-8 h-1/2 w-1/2 text-center mt-2 mb-2">
-                    <div className="capitalize font-extrabold text-white text-lg">{ title }</div>
-                 {links.map((link,linkIdx) => (
-                     <a className="mt-2 capitalize font-thin text-white hover:text-blue-400" href={link.url}>{link.title}</a>
-                 ) )}
-            </div>
+    return (
+        <div className="flex flex-col p-t-8 h-1/2 w-1/2 text-center mt-2 mb-2">
+            <div className="capitalize font-extrabold text-white text-lg">{title}</div>
+            {links.map((link, linkIdx) => (
+                <a className="mt-2 capitalize font-thin text-white hover:text-blue-400" href={link.url}>{link.title}</a>
+            ))}
+        </div>
     );
-    
+
 }
 
 const Footer = () => {
-    
+
     console.log(FOOTER_LINKS);
     return (
-        <div className="w-full h-auto relative mt-32 mb-0 mx-auto p-8 text-white" style={{backgroundColor:'#1868b7'}}>
-                <div>
-                    <div className="flex flex-wrap border-b border-gray-300 border-opacity-25 pb-10">
-                        <div className="flex flex-col md:w-5/8 md:pb-8 md:text-left md:pl-20">
-                            <div className="text-left text-white font-bold my-8 mx-0 text-2xl">Stay in the Loop</div>
-                            <p className="text-left md:text-left font-normal text-xl">Join our mailing list to get the latest updates on NFT and the best deals directly in your inbox</p>
-                            <div className="flex mt-2">
-                                <input className="p-3 md:w-3/4 rounded outline-none text-black text-xl" type="text" placeholder="Your email address"/>
-                                <div style={{backgroundColor : "#2081e2"}} className="rounded pr-6 pl-6 cursor-pointer text-center text-xl items-center justify-center inline-flex ml-2 md:w-1/4">    
-                                    Sign Up
+        <div className="w-full h-auto relative mt-32 mb-0 mx-auto p-8 text-white" style={{ backgroundColor: '#1868b7' }}>
+            <div>
+                <div className="flex flex-wrap border-b border-gray-300 border-opacity-25 pb-10">
+                    <div className="flex flex-col md:w-5/8 md:pb-8 md:text-left md:pl-20">
+                        <div className="text-left text-white font-bold my-8 mx-0 text-2xl">Stay in the Loop</div>
+                        <p className="text-left md:text-left font-normal text-xl">Join our mailing list to get the latest updates on NFT and the best deals directly in your inbox</p>
+                        <div className="flex mt-2">
+                            <input className="p-3 md:w-3/4 rounded outline-none text-black text-xl" type="text" placeholder="Your email address" />
+                            <div style={{ backgroundColor: "#2081e2" }} className="rounded pr-6 pl-6 cursor-pointer text-center text-xl items-center justify-center inline-flex ml-2 md:w-1/4">
+                                Sign Up
+                            </div>
+                        </div>
+                    </div>
+                    <div className="mt-5 md:mt-0 flex flex-col md:w-1/4 md:pb-8 md:text-left md:ml-40">
+                        <div className="text-left text-white font-bold my-8 mx-0 text-2xl">Join the Community</div>
+                        <div className="flex flex-wrap justify-start">
+                            <a href="https://twitter.com/" className="" target="_blank">
+                                <div className="p-2 rounded sm:p-3" style={{ backgroundColor: "#2081e2" }}>
+                                    <i class="fab fa-twitter fa-2x px-2 pr-2" style={{ color: "white" }}></i>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="mt-5 md:mt-0 flex flex-col md:w-1/4 md:pb-8 md:text-left md:ml-40">
-                            <div className="text-left text-white font-bold my-8 mx-0 text-2xl">Join the Community</div>
-                            <div className="flex flex-wrap justify-start">
-                                <a href="https://twitter.com/" className="" target="_blank">
-                                    <div className="p-3 rounded" style={{backgroundColor : "#2081e2"}}>
-                                        <i class="fab fa-twitter fa-2x px-2 pr-2" style={{color:"white"}}></i>
-                                    </div>
-                                </a>
-                                <a href="https://www.instagram.com/" className="ml-2" target="_blank">
-                                    <div className="p-3 rounded" style={{backgroundColor : "#2081e2"}}>
-                                    <i class="fab fa-instagram fa-2x px-2 pr-2" style={{color:"white"}}></i>
-                                    </div>
-                                </a>
-                                <a href="https://www.google.com/gmail/" className="ml-2" target="_blank">
-                                    <div className="p-3 rounded" style={{backgroundColor : "#2081e2"}}>
-                                        <i class="fas fa-envelope fa-2x px-2 pr-2" style={{color:"white"}}></i>
-                                    </div>
-                                </a>
-                                <a href="https://www.reddit.com/" className="ml-2" target="_blank">
-                                    <div className="p-3 rounded" style={{backgroundColor : "#2081e2"}}>
-                                        <i class="fab fa-reddit-alien fa-2x px-2 pr-2" style={{color:"white"}}></i>
-                                    </div>
-                                </a>
-                                <a href="https://discord.com/" className="ml-2" target="_blank">
-                                    <div className="p-3 rounded" style={{backgroundColor : "#2081e2"}}>
-                                    <i class="fab fa-discord fa-2x px-2 pr-2" style={{color:"white"}}></i>
-                                    </div>
-                                </a>
-                            </div>
+                            </a>
+                            <a href="https://www.instagram.com/" className="ml-2" target="_blank">
+                                <div className="p-2 rounded sm:p-3" style={{ backgroundColor: "#2081e2" }}>
+                                    <i class="fab fa-instagram fa-2x px-2 pr-2" style={{ color: "white" }}></i>
+                                </div>
+                            </a>
+                            <a href="https://www.google.com/gmail/" className="ml-2" target="_blank">
+                                <div className="p-2 rounded sm:p-3" style={{ backgroundColor: "#2081e2" }}>
+                                    <i class="fas fa-envelope fa-2x px-2 pr-2" style={{ color: "white" }}></i>
+                                </div>
+                            </a>
+                            <a href="https://www.reddit.com/" className="ml-2" target="_blank">
+                                <div className="p-2 rounded sm:p-3" style={{ backgroundColor: "#2081e2" }}>
+                                    <i class="fab fa-reddit-alien fa-2x px-2 pr-2" style={{ color: "white" }}></i>
+                                </div>
+                            </a>
+                            <a href="https://discord.com/" className="ml-2" target="_blank">
+                                <div className="p-2 rounded sm:p-3 " style={{ backgroundColor: "#2081e2" }}>
+                                    <i class="fab fa-discord fa-2x px-2 pr-2" style={{ color: "white" }}></i>
+                                </div>
+                            </a>
                         </div>
                     </div>
-                    <div className="flex flex-col md:flex-row p-b-8 border-b border-gray-300 border-opacity-25 flex-wrap mt-8 mb-8 pb-8 md:pl-20 items-center">
-                        <div className="flex flex-col md:w-1/2 md:text-left text-center inline-block align-middle">
-                            <div className="text-xl md:text-2xl md:text-center">NFT Original</div>
-                            <p className="mt-2 text-lg text justify">The world’s first and largest digital marketplace for crypto collectibles and non-fungible tokens (NFTs). Buy, sell, and discover exclusive digital assets.  NFTs have exciting new properties: they’re unique, provably scarce, liquid, and usable across multiple applications. Just like physical goods, you can do whatever you want with them!</p>
-                        </div>
-                        <div className="mt-8 flex flex-nowrap md:w-1/2 items-start justify-start">
-                            { FOOTER_LINKS.map((section,sectionIdx) => 
-                                { return (<Links title={section.section.title} links={section.section.links} key={sectionIdx} /> )})
-                            }
-                        </div>
+                </div>
+                <div className="flex flex-col md:flex-row p-b-8 border-b border-gray-300 border-opacity-25 flex-wrap mt-8 mb-8 pb-8 md:pl-20 items-center">
+                    <div className="flex flex-col md:w-1/2 md:text-left text-center inline-block align-middle">
+                        <div className="text-xl md:text-2xl md:text-center">NFT Original</div>
+                        <p className="mt-2 text-lg text justify">The world’s first and largest digital marketplace for crypto collectibles and non-fungible tokens (NFTs). Buy, sell, and discover exclusive digital assets.  NFTs have exciting new properties: they’re unique, provably scarce, liquid, and usable across multiple applications. Just like physical goods, you can do whatever you want with them!</p>
                     </div>
-                    <div className="flex md:flex-col content-center flex-wrap m-t-8 m-b-8 text-center justify-center item-center">
-                        <div>© 2018-2021 NFT ORIGINAL</div>
+                    <div className="mt-8 flex flex-nowrap md:w-1/2 items-start justify-start">
+                        {FOOTER_LINKS.map((section, sectionIdx) => { return (<Links title={section.section.title} links={section.section.links} key={sectionIdx} />) })
+                        }
                     </div>
+                </div>
+                <div className="flex md:flex-col content-center flex-wrap m-t-8 m-b-8 text-center justify-center item-center">
+                    <div>© 2018-2021 NFT ORIGINAL</div>
+                </div>
             </div>
         </div>
     );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ import '../CSS/header.css';
 
 
 const Header = () => {
-    
+
     return (
         <div>
             {/* <div className="flex text-2xl flex-col items-center w-full text-center mb-8 justify-center" style={{minHeight:'500px',backgroundImage:"url('https://opensea.io/static/images/frontpage-background.webp')",backgroundPosition:'center 150px',backgroundRepeat:'no-repeat',backgroundSize:'cover'}}>
@@ -14,9 +14,9 @@ const Header = () => {
                     <div className="py-1 px-8 bg-white border border-blue-400 text-blue-400 font-semibold rounded-md cursor-pointer hover:bg-blue-400 hover:text-white">Create</div>
                 </div>
             </div> */}
-            <div className="flex text-2xl flex-col w-full mb-8 bg-cover bg-no-repeat" style={{minHeight:'500px',backgroundImage:"url('https://cdn.shopify.com/s/files/1/1414/2472/articles/46_1300x800.png?v=1593590858')",boxShadow:"inset 0 0 0 1000px rgb(0 0 0 / 33%)"}}>
+            <div className="flex text-2xl flex-col w-full mb-8 bg-cover bg-no-repeat" style={{ minHeight: '100vh', backgroundImage: "url('https://cdn.shopify.com/s/files/1/1414/2472/articles/46_1300x800.png?v=1593590858')", boxShadow: "inset 0 0 0 1000px rgb(0 0 0 / 33%)" }}>
                 <div className="ml-8 mt-8">
-                    <div className="text-7xl text-white" style={{lineHeight:'5rem'}}>Discover<br></br> rare digital art<br></br> and collect NFTs</div>
+                    <div className="text-7xl text-white" style={{ lineHeight: '5rem' }}>Discover<br></br> rare digital art<br></br> and collect NFTs</div>
                     <div className="flex mt-8">
                         <a href="/browse" className="py-1 px-8 bg-blue-400 text-white font-semibold rounded-md mr-4 cursor-pointer hover:bg-white hover:text-blue-400 btn1">
                             Explore
@@ -26,7 +26,7 @@ const Header = () => {
                         </a>
                     </div>
                 </div>
-                
+
             </div>
         </div>
     )
