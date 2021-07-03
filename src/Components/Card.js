245c767c941b255acfe4ef74f63ac3c84cf3f5ad
Viraj Patidar <vrj381@gmail.com>
2021-07-03 11:13:16 +0530

    Card issue fixed

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -11,44 +11,43 @@ const Card = ({data}) => {
     const [isLoaded,setLoaded] = useState(false);
 
     return (
-        <Link to={{
-            pathname: "/asset/",
-            state: {
-                ownerId: data.ownerId == null ? "": data.ownerId.account_address[0],
-                name: data.assetName,
-                descr: data.description,
-                assetId: data.meta == null ? "" : data.meta.assetId
-            }
-        }}>
-        <div className="mb-4 p-4 bg-white rounded-md shadow-md w-96 border transform hover:-translate-y-1.5 hover:shadow-2xl duration-150 mx-2">
-            <div className="flex flex-row justify-end p-1.5">
-                {/* <FavoriteBorderIcon className="hover:text-red-500" /> */}
-                <i class="far fa-heart hover:text-red-500"></i>
-            <span className="pl-1">{data.likes}</span>
-            {/* <span className="pl-1">4</span> */}
-            </div>
-            <div className="relative">
-                <img className="rounded w-full h-64 object-contain" src={data.assetUrl} onLoad={setLoaded} />
-                {!isLoaded && <div className="absolute w-full h-full t-0 bg-white z-10">{data.assetUrl}</div> }
-                </div>
+            <Link to={{
+                pathname: "/asset/",
+                state: {
+                    ownerId: data.ownerId == null ? "": data.ownerId.account_address[0],
+                    name: data.assetName,
+                    descr: data.description,
+                    assetId: data.meta == null ? "" : data.meta.assetId
+                }
+            }}>
+                <div className="mb-4 p-4 bg-white rounded-md shadow-md w-96 border transform hover:-translate-y-1.5 hover:shadow-2xl duration-150 mx-2">
+                    <div className="flex flex-row justify-end p-1.5">
+                        {/* <FavoriteBorderIcon className="hover:text-red-500" /> */}
+                        <i class="far fa-heart hover:text-red-500"></i>
+                        <span className="pl-1">{data.likes}</span>
+                        {/* <span className="pl-1">4</span> */}
+                    </div>
+                    <div className="relative">
+                        <img className="rounded w-full h-64 object-contain" src={data.assetUrl || data.imageUrl} onLoad={setLoaded} />
+                        {!isLoaded && <div className="absolute w-full h-full t-0 bg-white z-10">{data.assetUrl}</div> }
+                    </div>
                     <div className="flex justify-between mt-2">
                         <div>
-                        <span className="block text-gray-400">{data.assetName}</span>
-                        <span>{data.assetName}</span>
-                        {/* <span>Title</span> */}
+                            <span className="block text-gray-400">{data.assetName}</span>
+                            <span>{data.assetName}</span>
+                            {/* <span>Title</span> */}
                         </div>
                         <div>
-                        <span className="block text-gray-400">Price</span>
+                            <span className="block text-gray-400">Price</span>
                             <div className="flex flex-row flex-nowrap items-center justify-evenly">
                                 <span className="font-bold">Ξ</span>
                                 <span className="font-bold">{price}</span>
-                                
                             </div>
                         </div>
                     </div>
-            </div>
+                </div>
             </Link>
-                )
+            )
 
 }
 
